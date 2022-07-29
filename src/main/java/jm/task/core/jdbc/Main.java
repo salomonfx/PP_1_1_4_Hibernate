ed1751b5e1a594d035b70b1aa6002eb3f9262f9b
salomonfx <99074676+salomonfx@users.noreply.github.com>
2022-07-29 18:32:06 +0300

    Доработал под jdbc

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 
 public class Main {
+    public final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 20);
+        userService.saveUser("Петр", "Петров", (byte) 26);
+        userService.saveUser("Борис", "Борисов", (byte) 34);
+        userService.saveUser("Заур", "Трегулов", (byte) 37);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String HOST = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=Europe/Moscow";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(HOST, LOGIN, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
+
+
